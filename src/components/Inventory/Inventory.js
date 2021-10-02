28d61fc20a24244538ea97d2cd684c5c650acde3
Tamim-uddin <muhammadtamimuddin@gmail.com>
2021-10-02 22:57:10 +0600

    update inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const inventory = () => {
     return (
         <div>
-            <h3>this is invetory </h3>
+            <h3>this is invetory Page</h3>
         </div>
     );
 };
