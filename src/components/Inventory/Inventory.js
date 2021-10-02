3fa952836c52c67247f5d82bb1e56b86dcb51174
Tamim-uddin <muhammadtamimuddin@gmail.com>
2021-10-02 17:34:13 +0600

    added inventory

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const inventory = () => {
+    return (
+        <div>
+            <h3>this is invetory </h3>
+        </div>
+    );
+};
+
+export default inventory;
